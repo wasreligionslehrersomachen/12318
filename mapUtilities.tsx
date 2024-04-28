71d281dbe4ed5069ab69ddca2da73ee0510eb692
wasreligionslehrersomachen <77389934+wasreligionslehrersomachen@users.noreply.github.com>
2024-04-28 07:18:47 +0200

    Create mapUtilities.tsx

---
+++ mapUtilities.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="tileset/mapUtilities.png" width="288" height="32"/>
+</tileset>
